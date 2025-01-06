636289cb4fe4bdc2e3855e4776fdb453b6ee0785
Jahaziel <jahzerahj@gmail.com>
2025-01-06 14:28:53 +0100

    button clicks

--- js/index.js
+++ js/index.js
@@ -12,11 +12,30 @@ document.querySelectorAll(".bookmark").forEach((bookmark) => {
         "src",
         currentSrc === initialSrc ? toggledSrc : initialSrc
       );
-  
       //Toggle active state on the parent question card
       const questionCard = bookmark.closest(".question-card");
       if (questionCard) {
         questionCard.classList.toggle("active");
       }
+    });
+  });
+
+  document.querySelectorAll(".button").forEach((button) => {
+    button.addEventListener("click", () => {
+    //   const currentSrc = bookmark.getAttribute("src");
+    //   const initialSrc = bookmark.getAttribute("data-initial");
+    //   const toggledSrc = bookmark.getAttribute("data-toggled");
+  
+    //   // Toggle image source
+    //   bookmark.setAttribute(
+    //     "src",
+    //     currentSrc === initialSrc ? toggledSrc : initialSrc
+    //   );
+    //   //Toggle active state on the parent question card
+    //   const questionCard = bookmark.closest(".question-card");
+    //   if (questionCard) {
+    //     questionCard.classList.toggle("active");
+    //   }
+    console.log('clicky')
     });
   });
